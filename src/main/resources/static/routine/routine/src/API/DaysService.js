06a6bc86224ff940abb5a7bd9cc9a7f2cec3ab9e
MorgothGorthaur <victortarasov13032001@gmail.com>
2022-08-06 01:25:40 +0300

    change front

--- src/main/resources/static/routine/routine/src/API/DaysService.js
+++ src/main/resources/static/routine/routine/src/API/DaysService.js
@@ -1,9 +1,16 @@
-import axios from "axios";
+/*import axios from "axios";*/
 import React, {useState} from 'react';
 export default class DaysService {
   static async getAll(){
-    const response = await axios.get('http://localhost:8080/routine');
-    return response.data;
+    /*const response = await axios.get('http://localhost:8080/routine');
+    return response.data;*/
+    try{
+	const response = await fetch('http://localhost:8080/routine');
+	const data = await response.json();
+	return data;
+    } catch (e) {
+	alert(e);
+    }
   }
 
   static async addDay(name, date){
