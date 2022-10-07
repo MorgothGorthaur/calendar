11c139c31627f50070cd24a55141d59d1d690e51
MorgothGorthaur <victortarasov13032001@gmail.com>
2022-10-07 22:18:48 +0300

    fixed scheduled methods

--- src/main/java/com/example/routine/Service/DayActualityManager.java
+++ src/main/java/com/example/routine/Service/DayActualityManager.java
@@ -4,17 +4,19 @@ import com.example.routine.Model.DayActuality;
 import com.example.routine.Repository.DayRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 
 @AllArgsConstructor
+@Component
 public class DayActualityManager {
     private DayRepository dayRepository;
     /*
      * every day sets day`s actuality status, and remove passed days
      */
     @Scheduled(cron = "${dayActualityManagerCronExpression}")
-    public void updateDayActuality() {
+    private void updateDayActuality() {
         try {
 
             var actualDate = LocalDate.now();
--- src/main/java/com/example/routine/Service/TimeActualityManager.java
+++ src/main/java/com/example/routine/Service/TimeActualityManager.java
@@ -3,18 +3,27 @@ package com.example.routine.Service;
 import com.example.routine.Repository.DayRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 @AllArgsConstructor
+@Component
 public class TimeActualityManager {
     private DayRepository dayRepository;
     @Scheduled(cron="${timeActualityManagerCronExpression}")
-    public void updateTimeActuality(){
-        System.err.println("d");
+    private void updateTimeActuality(){
         try {
-            dayRepository.findDayByDate(LocalDate.now()).ifPresent(day -> day.getEvents().removeIf(event -> event.getTime().isBefore(LocalTime.now())));
+            var day = dayRepository.findDayByDate(LocalDate.now()).orElse(null);
+            if(day != null && day.getEvents() != null){
+                for(var event : day.getEvents()){
+                    if( event.getTime().isBefore(LocalTime.now())){
+                        day.removeEvent(event);
+                        dayRepository.save(day);
+                    }
+                }
+            }
         } catch ( Exception ex) {
             throw ex;
         }
