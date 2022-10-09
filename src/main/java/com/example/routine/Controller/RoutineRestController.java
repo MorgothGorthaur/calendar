c5bf2ca6a5cce4a15380236270f5aada75db2dbd
MorgothGorthaur <victortarasov13032001@gmail.com>
2022-10-09 17:52:43 +0300

    remove some errors

--- src/main/java/com/example/routine/Controller/RoutineRestController.java
+++ src/main/java/com/example/routine/Controller/RoutineRestController.java
@@ -9,7 +9,6 @@ import javax.validation.Valid;
 
 import com.example.routine.DTO.ParticipantDto;
 import com.example.routine.DTO.ParticipantFullDto;
-import com.example.routine.Model.Participant;
 import com.example.routine.Model.ParticipantStatus;
 import com.example.routine.Repository.EventRepository;
 import com.example.routine.Repository.ParticipantRepository;
@@ -30,39 +29,39 @@ import com.example.routine.Model.Event;
 public class RoutineRestController {
 
     private EventRepository eventRepository;
-    private ParticipantRepository particioantRepository;
+    private ParticipantRepository participantRepository;
     private ModelMapper modelMapper;
 
     @GetMapping
     public ResponseEntity<List<ParticipantDto>> findAll() {
-        return ResponseEntity.ok(particioantRepository.findParticipantByStatus(ParticipantStatus.ACTIVE).stream().map(participant -> modelMapper.map(participant, ParticipantDto.class)).collect(Collectors.toList()));
+        return ResponseEntity.ok(participantRepository.findParticipantByStatus(ParticipantStatus.ACTIVE).stream().map(participant -> modelMapper.map(participant, ParticipantDto.class)).collect(Collectors.toList()));
     }
 
     @PostMapping
     public ResponseEntity<ParticipantDto> addParticipant(@Valid @RequestBody ParticipantDto participantDto) {
         var participant = participantDto.toParticipant();
-        return ResponseEntity.ok(modelMapper.map(particioantRepository.save(participant), ParticipantDto.class));
+        return ResponseEntity.ok(modelMapper.map(participantRepository.save(participant), ParticipantDto.class));
     }
 
     @GetMapping("/{participantId}")
     public ResponseEntity<ParticipantFullDto> getWithEvents(@PathVariable Long participantId) {
-        var participant = particioantRepository.findByIdAndStatus(participantId, ParticipantStatus.ACTIVE).orElseThrow(() -> new ParticipantNotFoundException(participantId));
+        var participant = participantRepository.findByIdAndStatus(participantId, ParticipantStatus.ACTIVE).orElseThrow(() -> new ParticipantNotFoundException(participantId));
         return ResponseEntity.ok(modelMapper.map(participant, ParticipantFullDto.class));
     }
 
     @DeleteMapping("/{participantId}")
     public ResponseEntity<String> deleteParticipant(@PathVariable Long participantId){
-        var participant = particioantRepository.findByIdAndStatus(participantId, ParticipantStatus.ACTIVE).orElseThrow(() -> new ParticipantNotFoundException(participantId));
+        var participant = participantRepository.findByIdAndStatus(participantId, ParticipantStatus.ACTIVE).orElseThrow(() -> new ParticipantNotFoundException(participantId));
         participant.setStatus(ParticipantStatus.REMOVED);
-        particioantRepository.save(participant);
+        participantRepository.save(participant);
         return ResponseEntity.ok("deleted");
     }
     @PatchMapping()
     public ResponseEntity<ParticipantDto> changeParticipant(@Valid @RequestBody ParticipantDto participantDto){
-        var participant = particioantRepository.findByIdAndStatus(participantDto.getId(), ParticipantStatus.ACTIVE).orElseThrow(()-> new ParticipantNotFoundException(participantDto.getId()));
+        var participant = participantRepository.findByIdAndStatus(participantDto.getId(), ParticipantStatus.ACTIVE).orElseThrow(()-> new ParticipantNotFoundException(participantDto.getId()));
         participant.setLastName(participantDto.getLastName());
         participant.setFirstName(participantDto.getFirstName());
-        particioantRepository.save(participant);
+        participantRepository.save(participant);
         return ResponseEntity.ok(participantDto);
     }
     @PostMapping("/events")
--- src/main/java/com/example/routine/RoutineApplication.java
+++ src/main/java/com/example/routine/RoutineApplication.java
@@ -9,7 +9,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 public class RoutineApplication {
     public static void main(String[] args) {
-        System.out.println("ddddd");
         SpringApplication.run(RoutineApplication.class, args);
     }
 }
