1ec8307a899cd459296c05dfaab79f4c23f2166a
MorgothGorthaur <victortarasov13032001@gmail.com>
2022-10-12 23:58:37 +0300

    remove old files

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM openjdk:17-oracle 
-ADD target/routine-0.0.1-SNAPSHOT.jar routine-0.0.1-SNAPSHOT.jar
-EXPOSE 8080
-ENTRYPOINT ["java", "-jar", "routine-0.0.1-SNAPSHOT.jar"]
-
--- routine/routine/src/UI/Input/Input.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import cl from './Input.module.css'
-const Input = (props) =>{
-  return (
-      <input className = { cl.Input }{...props} />
-  )
-};
-export default Input;
