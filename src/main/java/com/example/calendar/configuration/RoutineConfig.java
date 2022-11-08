e35bfbe4838b5484349130947370a75228b11cdf
MorgothGorthaur <victortarasov13032001@gmail.com>
2022-11-08 03:13:00 +0200

    return removed dependency

--- src/main/java/com/example/calendar/configuration/RoutineConfig.java
+++ src/main/java/com/example/calendar/configuration/RoutineConfig.java
@@ -1,15 +1,8 @@
 package com.example.calendar.configuration;
 
-import javax.validation.Validation;
-import javax.validation.Validator;
-import javax.validation.ValidatorFactory;
 
-import org.hibernate.validator.HibernateValidator;
-import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.validation.beanvalidation.SpringConstraintValidatorFactory;
 import org.modelmapper.ModelMapper;
 @Configuration
 public class RoutineConfig {
