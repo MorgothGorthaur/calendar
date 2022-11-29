5f2c95331c427e48c551f9a059d6f90f4f3d090d
MorgothGorthaur <victortarasov13032001@gmail.com>
2022-11-30 01:58:23 +0200

    add findParticipantsWithEqualEmailAndNonEqualId to participant repository

--- src/main/java/com/example/calendar/Repository/ParticipantRepository.java
+++ src/main/java/com/example/calendar/Repository/ParticipantRepository.java
@@ -3,6 +3,7 @@ package com.example.calendar.Repository;
 import com.example.calendar.Model.Participant;
 import com.example.calendar.Model.ParticipantStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,4 +11,6 @@ import java.util.Optional;
 public interface ParticipantRepository extends JpaRepository<Participant, Long> {
     List<Participant> findParticipantByStatus(ParticipantStatus status);
     Optional<Participant> findByEmail(String email);
+    @Query(value = "select p from Participant p where p.email = ?1 and p.id <> ?2")
+    List<Participant> findParticipantsWithEqualEmailAndNonEqualId(String email, Long id);
 }
